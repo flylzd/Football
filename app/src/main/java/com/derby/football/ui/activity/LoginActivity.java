4daa3963a7527f2cf1ba3d8f45cefee55b4d321f
lizeda <123456>
2016-02-03 01:08:09 +0800

    init

--- app/src/main/java/com/derby/football/ui/activity/LoginActivity.java
+++ app/src/main/java/com/derby/football/ui/activity/LoginActivity.java
@@ -1,8 +1,14 @@
 package com.derby.football.ui.activity;
 
 
+import com.derby.football.R;
 import com.derby.football.base.BaseActivity;
 
 public class LoginActivity extends BaseActivity {
 
+
+    @Override
+    protected int getLayoutId() {
+        return R.layout.activity_login;
+    }
 }
